330361b2620683df793917ac4b499db6cb16327c
Junavae <127152553+Junavae@users.noreply.github.com>
2024-07-12 01:31:46 -0500

    Add print function to cube number with text

--- Some_Expressions.py
+++ Some_Expressions.py
@@ -1,3 +1,4 @@
 user_num = int(input('Enter integer:\n'))
 print('You entered:', user_num)
 print(user_num, 'squared is', user_num * user_num)
+print('And', user_num, 'cubed is', user_num * user_num * user_num, '!!')
