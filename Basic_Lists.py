84e1c2ef13d538d39096fba9c8ffda2339096d4c
Junavae <127152553+Junavae@users.noreply.github.com>
2024-07-13 21:09:11 -0500

    Add notes

--- Basic_Lists.py
+++ Basic_Lists.py
@@ -1,11 +1,11 @@
 short_names = ['Chris', 'Hank', 'Joel']
-short_names.append('Bruce')
+short_names.append('Bruce') # Add to end of list
 print(short_names)
-short_names.pop(3)
+short_names.pop(3)          # Remove element at index
 print(short_names)
 short_names.append('Rusty')
 print(short_names)
-short_names.remove('Rusty')
+short_names.remove('Rusty') # Remove element with value 'x'
 print(short_names)
 print(short_names[0])
 print(short_names[1])
