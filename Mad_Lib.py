ee6eb5d1e7fa4eef93cddceae3f42a25916443d9
Junavae <127152553+Junavae@users.noreply.github.com>
2024-07-13 19:57:20 -0500

    Adjust print function to multiple lines for readability

--- Mad_Lib.py
+++ Mad_Lib.py
@@ -2,4 +2,7 @@ first_name = input()
 generic_location = input()
 whole_number = input()
 plural_noun = input()
-print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
+# result can be printed on one line or multiple for readability
+print(first_name, 'went to', generic_location)
+print('to buy', whole_number, 'different types of')
+print(plural_noun)
