221174269902e35141f7308acf29ea1bcf6f2e74
Junavae <127152553+Junavae@users.noreply.github.com>
2024-07-13 21:14:34 -0500

    Add new print function & add text to print functions

--- Basic_Lists.py
+++ Basic_Lists.py
@@ -1,12 +1,13 @@
 short_names = ['Chris', 'Hank', 'Joel']
+print(short_names)
 short_names.append('Bruce') # Add to end of list
-print(short_names)
+print('After append: ', short_names)
 short_names.pop(3)          # Remove element at index
-print(short_names)
+print('After .pop removal: ', short_names)
 short_names.append('Rusty')
-print(short_names)
+print('After append: ', short_names)
 short_names.remove('Rusty') # Remove element with value 'x'
-print(short_names)
+print('After .remove removal: ', short_names)
 print(short_names[0])
 print(short_names[1])
 print(short_names[2])
