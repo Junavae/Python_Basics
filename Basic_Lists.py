4f4b8c1ada5b97eb51934d5678aa3d3f6c87a63a
Junavae <127152553+Junavae@users.noreply.github.com>
2024-07-13 21:00:23 -0500

    Add method-remove appended element, and a print function

--- Basic_Lists.py
+++ Basic_Lists.py
@@ -1,6 +1,8 @@
 short_names = ['Chris', 'Hank', 'Joel']
 short_names.append('Bruce')
 print(short_names)
+short_names.pop(3)
+print(short_names)
 print(short_names[0])
 print(short_names[1])
 print(short_names[2])
