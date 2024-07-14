80c8c06b452d17559dc618318bfd94ab854d54e7
Junavae <127152553+Junavae@users.noreply.github.com>
2024-07-13 20:14:26 -0500

    Alter text for 'user_num' variable

--- String_Indexing.py
+++ String_Indexing.py
@@ -1,3 +1,3 @@
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-user_num = int(input('Enter a number to use as an index'))
+user_num = int(input('Enter a number to use as an index: '))
 print('\nLetter', user_num, 'of the alphabet is', alphabet[user_num])
